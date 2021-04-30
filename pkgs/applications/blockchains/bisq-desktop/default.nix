ed4bcc02cbcacca1d0377505360293c5e818503c
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2021-04-30 15:26:48 +0700

    bisq: move launcher.nix into default.nix

--- pkgs/applications/blockchains/bisq-desktop/default.nix
+++ pkgs/applications/blockchains/bisq-desktop/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , lib
-, callPackage
 , makeWrapper
 , fetchurl
 , makeDesktopItem
@@ -8,9 +7,43 @@
 , imagemagick
 , openjdk11
 , dpkg
+, writeScript
+, coreutils
+, bash
+, tor
+, psmisc
 }:
 let
-  bisq-launcher = callPackage ./launcher.nix { };
+  bisq-launcher = writeScript "bisq-launcher" ''
+    #! ${bash}/bin/bash
+
+    # Setup a temporary Tor instance
+    TMPDIR=$(${coreutils}/bin/mktemp -d)
+    CONTROLPORT=$(${coreutils}/bin/shuf -i 9100-9499 -n 1)
+    SOCKSPORT=$(${coreutils}/bin/shuf -i 9500-9999 -n 1)
+    ${coreutils}/bin/head -c 1024 < /dev/urandom > $TMPDIR/cookie
+
+    ${tor}/bin/tor --SocksPort $SOCKSPORT --ControlPort $CONTROLPORT \
+      --ControlPortWriteToFile $TMPDIR/port --CookieAuthFile $TMPDIR/cookie \
+      --CookieAuthentication 1 >$TMPDIR/tor.log --RunAsDaemon 1
+
+    torpid=$(${psmisc}/bin/fuser $CONTROLPORT/tcp)
+
+    echo Temp directory: $TMPDIR
+    echo Tor PID: $torpid
+    echo Tor control port: $CONTROLPORT
+    echo Tor SOCKS port: $SOCKSPORT
+    echo Tor log: $TMPDIR/tor.log
+    echo Bisq log file: $TMPDIR/bisq.log
+
+    JAVA_TOOL_OPTIONS="-XX:MaxRAM=4g" bisq-desktop-wrapped \
+      --torControlCookieFile=$TMPDIR/cookie \
+      --torControlUseSafeCookieAuth \
+      --torControlPort $CONTROLPORT "$@" > $TMPDIR/bisq.log
+
+    echo Bisq exited. Killing Tor...
+    kill $torpid
+  '';
 in
 stdenv.mkDerivation rec {
   version = "1.6.2";
