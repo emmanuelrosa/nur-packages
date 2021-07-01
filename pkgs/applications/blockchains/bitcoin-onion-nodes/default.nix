3400e6105aa933c7974c63e3de18a752200f0353
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2021-07-01 08:40:16 +0700

    bitcoin-onion-nodes: update

--- pkgs/applications/blockchains/bitcoin-onion-nodes/default.nix
+++ pkgs/applications/blockchains/bitcoin-onion-nodes/default.nix
@@ -3,16 +3,16 @@
 }:
 
 let
-  description = "A list of over 200 Bitcoin Core nodes running as Tor v3 onion services.";
+  description = "A list of over 400 Bitcoin Core nodes running as Tor v3 onion services.";
 in stdenv.mkDerivation rec {
   name = "bitcoin-onion-nodes-${version}.txt";
-  version = "a3d30c0e967ba4b86040488c7a19a566ee8a2b5a";
+  version = "1a41129878e8ad9dd4a9a0149dbe0bcc918d04fb";
 
   src = fetchFromGitHub {
     owner = "emmanuelrosa";
     repo = "bitcoin-onion-nodes";
     rev = version;
-    sha256 = "1ipib498dxfr8y36nz90ljhbfqk95cfswh6cbrkrbwqrdr7a1iip";
+    sha256 = "1k4jjfk5s3xr6ykwkz8bb1ha027csn3r1yicchgmgkkhc93izppj";
   };
 
   ignoreFile = writeText "ignore.txt" ''
