14f6f4dd30db90cf0d1dcd7c8d8f725f889ceb43
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2021-09-09 12:30:02 +0700

    sparrow: use an extract JIMAGE
    TODO: get the module paths working.

--- pkgs/applications/blockchains/sparrow/default.nix
+++ pkgs/applications/blockchains/sparrow/default.nix
@@ -10,7 +10,14 @@
 , imagemagick
 }:
 
-stdenv.mkDerivation rec {
+let launcher = writeScript "sparrow" ''
+  #! ${bash}/bin/bash
+
+  cd $(dirname $(dirname $0))/lib
+  modules=$(ls | xargs | tr " " ":")
+  ${openjdk16}/bin/java -p $modules -m com.sparrowwallet.sparrow/com/sparrowwallet/sparrow/MainApp $@
+'';
+in stdenv.mkDerivation rec {
   pname = "sparrow";
   version = "1.4.3";
 
@@ -33,27 +40,20 @@ stdenv.mkDerivation rec {
   ];
 
   buildPhase = ''
-    # Replace the Java runtime with the one from Nixpkgs,
-    # but with some files from the sparrow tarball
-    mkdir myruntime
-    cp -r ${openjdk16}/lib/openjdk/* myruntime/
-    chmod u+w myruntime/bin
-    chmod u+w myruntime/lib
-    rm myruntime/lib/modules
-    cp lib/runtime/bin/sparrow myruntime/bin/
-    cp lib/runtime/lib/modules myruntime/lib/
+    # Extract Sparrow's JIMAGE and replace the OpenJDK modules.
+    mkdir mymodules
+    pushd mymodules
+    ${openjdk16}/bin/jimage extract ../lib/runtime/lib/modules
+    ${openjdk16}/bin/jimage extract ${openjdk16}/lib/openjdk/lib/modules
+    popd
   '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin $out/opt/${pname}/lib/runtime
-    cp -a bin $out/opt/${pname}
-    cp -a lib/app $out/opt/${pname}/lib
-    ln -s $out/opt/${pname}/bin/Sparrow $out/bin/${pname}
-    cp -a myruntime/* $out/opt/${pname}/lib/runtime/
-
-    patchelf --interpreter $(echo ${stdenv.glibc.out}/lib/ld-linux*) $out/opt/${pname}/bin/Sparrow
+    mkdir -p $out/bin $out/lib
+    cp -r mymodules/* $out/lib/
+    install -Dmu+x ${launcher} $out/bin/sparrow
 
     for n in 16 24 32 48 64 96 128 256; do
       size=$n"x"$n
